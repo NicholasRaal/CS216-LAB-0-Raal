6807508dd026064a693f62bc6ac507e4a27a1c90
NicholasRaal <raaln1120@student.laccd.edu>
2025-02-16 18:23:21 -0800

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+// Nicholas Raal
+// Lab #0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout << "\nMy name is Nicholas Raal" << endl;
+
+    cout << "\nEnter the first number ";
+    cin >> num2;
+
+    cout << "\nEnter the second number ";
+    cin >> num1;
+
+    sum = num1 + num2;
+    cout << "\nThe sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
+
+/*
+My name is Nicholas Raal
+
+Enter the first number 2
+
+Enter the second number 4
+
+The sum of 4 and 2 is 6
+*/
